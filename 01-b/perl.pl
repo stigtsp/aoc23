7ddf227e481da565808e32bc8a9a88628d7e659b
Stig Palmquist <git@stig.io>
2023-12-02 15:00:40 +0100

    day 01-b, perl: clean up indentation and stuff

--- 01-b/perl.pl
+++ 01-b/perl.pl
@@ -3,24 +3,19 @@
 use v5.38;
 use List::Util qw(sum);
 
-my %conv = qw(
-  one 1
-  two 2
-  three 3
-  four 4
-  five 5
-  six 6
-  seven 7
-  eight 8
-  nine 9
-);
+my %conv = qw( one   1
+               two   2
+               three 3
+               four  4
+               five  5
+               six   6
+               seven 7
+               eight 8
+               nine  9 );
 
-my $re = join("|", sort keys %conv);
+my $re = join "|", keys %conv;
 
-say sum
-  map {
+say sum map {
   join "",
-    map { $conv{$_} || $_ }
-    /(\d|$re)/,
-    /.*(\d|$re)/;
-  } <STDIN>;
+    map { $conv{$_} || $_ } /(\d|$re)/, /.*(\d|$re)/;
+} <STDIN>;
