3ca06c5aff761b309c74f6d0a3d35870bd880eed
Stig Palmquist <git@stig.io>
2023-12-02 14:50:28 +0100

    day 1-b, perl

---
+++ 01-b/perl.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use v5.38;
+use List::Util qw(sum);
+
+my %conv = qw(
+  one 1
+  two 2
+  three 3
+  four 4
+  five 5
+  six 6
+  seven 7
+  eight 8
+  nine 9
+);
+
+my $re = join("|", sort keys %conv);
+
+say sum
+  map {
+  join "",
+    map { $conv{$_} || $_ }
+    /(\d|$re)/,
+    /.*(\d|$re)/;
+  } <STDIN>;
